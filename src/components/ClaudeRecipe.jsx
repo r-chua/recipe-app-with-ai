d837089d189d8ef1ea7b5a71f62e807dc72a40a7
Royce <rvmchua@gmail.com>
2025-08-11 06:54:27 +0800

    Add scrollIntoView function for enhance UX

--- src/components/ClaudeRecipe.jsx
+++ src/components/ClaudeRecipe.jsx
@@ -1,8 +1,8 @@
 import ReactMarkdown from "react-markdown"
 
-export default function ClaudeRecipe({recipeFromAI}) {
+export default function ClaudeRecipe({recipeFromAI, recipeSection}) {
     return (
-        <section className="suggested-recipe-container" aria-live="polite">
+        <section className="suggested-recipe-container" aria-live="polite" ref={recipeSection}>
             <h2>The Chef Recommends:</h2>
             <ReactMarkdown children={recipeFromAI} />
         </section>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,19 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import ClaudeRecipe from "./ClaudeRecipe"
 import IngredientsList from "./IngredientsList"
 import { getRecipeFromMistral } from "../../ai"
 
 export default function Main() {
-    const [ingredients, setIngredients] = useState([])
+    const [ingredients, setIngredients] = useState(["chicken", "all the main spices", "corn", "heavy cream", "pasta"])
     const [showRecipe, setShowRecipe] = useState(false)
-    const [recipeFromAI, setRecipeFromAI] = useState()
+    const [recipeFromAI, setRecipeFromAI] = useState("")
+    const recipeSection = useRef(null)
+
+    useEffect(() => {
+        if (recipeFromAI !== "" && recipeSection.current !== null) {
+            recipeSection.current.scrollIntoView({behavior: "smooth"})
+        }
+    }, [recipeFromAI]) 
 
     function addIngredient(formData) {
         const newIngredient = formData.get("ingredient")
@@ -35,10 +42,12 @@ export default function Main() {
             <IngredientsList 
                 ingredients={ingredients}
                 getRecipe={getRecipe}
+                
             /> : null }
             
             {showRecipe ? <ClaudeRecipe
                 recipeFromAI={recipeFromAI}
+                ref={recipeSection}
             /> : null}
             
         </main>
